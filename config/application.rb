f5afef1bf51295710f522f71816ba9096606d805
Manu Phatak <manu@thinkcerca.com>
2018-01-14 11:07:56 -0600

    Initial Setup

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -29,5 +28,6 @@ module HadesApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_job.queue_adapter = :sucker_punch
   end
 end
